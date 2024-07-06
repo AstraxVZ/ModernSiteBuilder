6d2596f6f07e16c25f66cbcbeb6c14d25e4da159
_meuti <91541572+AstraxVZ@users.noreply.github.com>
2024-07-06 12:02:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const sections = [
+    { title: 'Section', content: 'It works!' },
+    { title: 'Section 2', content: 'This is the second section!' },
+];
+
+MSBuilder.createPage('Example Website', sections, 'footer test', true);
